8c71d0f165e9e9465154d6a03d011eebb0f4abd6
jmsemoon <148172069+jmsemoon@users.noreply.github.com>
2023-10-19 03:15:49 +0100

    Create Myfavoritesong.java

---
+++ PRELIM/Myfavoritesong.java
@@ -0,0 +1,11 @@
+//my favorite song
+public class Myfavoritesong
+{
+	public static void main(String[] args) {
+		System.out.println("Brown Eyes Song by Destiny's Child");
+		System.out.println("The way we held each other's hands");
+		System.out.println("The way we talked, the way we laughed");
+		System.out.println("It felt so good to fall in love");
+		System.out.println("And I knew right there and then that you were the one");
+	}
+}
